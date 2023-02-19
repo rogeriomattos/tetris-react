03b3d467e0801432b4a6efc276a6e915abb74ad0
Rogerio Almeida de Mattos <rogeriomattos38@outlook.com>
2023-02-19 00:27:48 -0300

    Criacao da func para randomizar a peca

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { Player } from "../types/player";
-import { TETROMINOS } from '../utlis/tetraminios';
+import { getRandomTetramino, TETROMINOS } from "../utlis/tetraminios";
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState<Player>({
         pos: { x: 0, y: 4 },
-        tetramino: TETROMINOS.O,
+        tetramino: getRandomTetramino(),
     });
 
     return {
